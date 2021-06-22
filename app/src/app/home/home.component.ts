3b0c65c4a03fd7192d952041b0c0c72787af66ef
cliff42 <cliff42@students.cs.ubc.ca>
2021-06-21 20:03:09 -0700

    adding base map ui

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  zoom = 12;
+  // center: google.maps.LatLngLiteral;
+  options: google.maps.MapOptions = {
+    mapTypeId: 'hybrid',
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8,
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+    // navigator.geolocation.getCurrentPosition((position) => {
+    //   this.center = {
+    //     lat: position.coords.latitude,
+    //     lng: position.coords.longitude,
+    //   }
+    // })
   }
 
 }
